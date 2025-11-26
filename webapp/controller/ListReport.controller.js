22f158f0d2904ab76e5bf3e33f5f62dd89da64cf
Danila Mikhaliou <Danila.Mikhaliou@LeverX.local>
2025-11-26 14:42:17 +0300

    'added_filter-deletion-ProductDeletion'

--- webapp/controller/ListReport.controller.js
+++ webapp/controller/ListReport.controller.js
@@ -1,62 +1,165 @@
-sap.ui.define(['productsreport/controller/BaseController'], (BaseController) => {
-  'use strict';
+sap.ui.define(
+  [
+    'productsreport/controller/BaseController',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
+    'sap/m/MessageBox',
+    'productsreport/utils/formatter',
+  ],
+  (BaseController, Filter, FilterOperator, MessageBox, Formatter) => {
+    'use strict';
 
-  return BaseController.extend('productsreport.controller.ListReport', {
-    onInit() {},
+    return BaseController.extend('productsreport.controller.ListReport', {
+      onInit() {},
 
-    onPressNavigateToDetails(oEvent) {
-      const productId = oEvent.getSource().getBindingContext('data').getProperty('ProductId');
-      this._navigate('ProductDetails', { productId });
-    },
+      onPressNavigateToDetails(oEvent) {
+        const productId = oEvent.getSource().getBindingContext('data').getProperty('ProductId');
+        this._navigate('ProductDetails', { productId });
+      },
 
-    async handleValueHelp() {
-      if (!this.oDialog) {
-        this.oDialog = await this.loadFragment({
-          name: 'productsreport.fragment.ValueHelpDialog',
-        });
+      async handleValueHelp() {
+        if (!this.oDialog) {
+          this.oDialog = await this.loadFragment({
+            name: 'productsreport.fragment.ValueHelpDialog',
+          });
 
-        const oTable = new sap.ui.table.Table({
-          visibleRowCount: 10,
-          selectionMode: 'Multi',
-        });
+          const oTable = new sap.ui.table.Table({
+            visibleRowCount: 10,
+            selectionMode: 'Multi',
+          });
 
-        oTable.addColumn(
-          new sap.ui.table.Column({
-            label: new sap.m.Label({ text: 'Producer Name' }),
-            template: new sap.m.Text({ text: '{data>ProducerName}' }),
-          }),
+          oTable.addColumn(
+            new sap.ui.table.Column({
+              label: new sap.m.Label({ text: 'Producer Name' }),
+              template: new sap.m.Text({ text: '{data>ProducerName}' }),
+            }),
+          );
+
+          oTable.bindRows('data>/Producers');
+
+          this.oDialog.setTable(oTable);
+        }
+
+        this.oDialog.setTokens(this.byId('multiInputWithValueHelp').getTokens());
+        this.oDialog.update();
+        this.oDialog.open();
+      },
+
+      onValueHelpOkPress(oEvent) {
+        const aTokens = oEvent.getParameter('tokens');
+        const oMultiInput = this.byId('multiInputWithValueHelp');
+        oMultiInput.setTokens(aTokens);
+        this.oDialog.close();
+        this.onSearchAction();
+      },
+
+      onCloseValueHelp() {
+        this.oDialog.close();
+      },
+
+      onAfterCloseValueHelp() {
+        this.oDialog.setTokens([]);
+      },
+
+      onSearchAction() {
+        const sValueSearchField = this.byId('searchFieldProducts').getValue();
+        const aValueComboBox = this.byId('comboBoxProducts').getSelectedItems();
+        const aValueMultiInput = this.byId('multiInputWithValueHelp').getTokens();
+
+        const aFilter = [];
+
+        aFilter.push(new Filter('ProductName', FilterOperator.Contains, sValueSearchField));
+        aValueComboBox.forEach((oValueComboBox) =>
+          aFilter.push(new Filter('Category', FilterOperator.EQ, oValueComboBox.getText())),
+        );
+        aValueMultiInput.forEach((oValueMultiInput) =>
+          aFilter.push(
+            new Filter('ProducerName', FilterOperator.EQ, oValueMultiInput.getText().slice(0, -4)),
+          ),
+        );
+        this.byId('idProductsTable').getBinding('items').filter(aFilter);
+      },
+
+      onMultiInputTokenUpdate(oEvent) {
+        const oTokensRemoved = oEvent.getParameter('removedTokens')[0];
+        const aSelectedTokens = this.byId('multiInputWithValueHelp').getTokens();
+        const aUpdatedTokens = aSelectedTokens.filter((token) => token.sId !== oTokensRemoved.sId);
+
+        this.byId('multiInputWithValueHelp').setTokens(aUpdatedTokens);
+        this.onSearchAction();
+      },
+
+      onSelectionChange(oEvent) {
+        if (oEvent.getSource().getSelectedItems().length) {
+          const oDeleteButton = this.byId('deleteProductBtn').setEnabled(true);
+
+          oEvent
+            .getSource()
+            .getSelectedItems()
+            .forEach(
+              (selectedProduct) => selectedProduct.getBindingContext('data').getObject().ProductId,
+            );
+        } else {
+          const oDeleteButton = this.byId('deleteProductBtn').setEnabled(false);
+        }
+      },
+
+      onDeleteProducts() {
+        const sProductDeleteConfirmationText = this.getView()
+          .getModel('i18n')
+          .getResourceBundle()
+          .getText('productDeleteConfirmation');
+        const sProductMultiDeleteConfirmationText = this.getView()
+          .getModel('i18n')
+          .getResourceBundle()
+          .getText('productMultiDeleteConfirmation');
+        const oProductModel = this.getView().getModel('data');
+        const oProductTable = this.byId('idProductsTable');
+        const aSelectedProducts = oProductTable
+          .getSelectedItems()
+          .map((oSelectedProduct) => oSelectedProduct.getBindingContext('data').getObject());
+
+        let aProducts = oProductModel.getProperty('/Products');
+
+        aSelectedProducts.forEach(
+          (oSelectedProduct) =>
+            (aProducts = aProducts.filter(
+              (oProduct) => oProduct.ProductId !== oSelectedProduct.ProductId,
+            )),
         );
 
-        oTable.bindRows('data>/Producers');
+        const sFormattedTextSingleConfirmation = Formatter.formatProductsCount(
+          sProductDeleteConfirmationText,
+          aSelectedProducts[0].ProductName,
+        );
 
-        this.oDialog.setTable(oTable);
-      }
+        const sFormattedTextMultiConfirmation = Formatter.formatProductsCount(
+          sProductMultiDeleteConfirmationText,
+          aSelectedProducts.length,
+        );
 
-      this.oDialog.setTokens(this.byId('multiInputWithValueHelp').getTokens());
-      this.oDialog.update();
-      this.oDialog.open();
-    },
-
-    onValueHelpOkPress(oEvent) {
-      const aTokens = oEvent.getParameter('tokens');
-      const oMultiInput = this.byId('multiInputWithValueHelp');
-      oMultiInput.setTokens(aTokens);
-      this.oDialog.close();
-    },
-
-    onCloseValueHelp() {
-      this.oDialog.close();
-    },
-
-    onAfterCloseValueHelp() {
-      this.oDialog.setTokens([]);
-    },
-
-    onMultiInputTokenUpdate(oEvent) {
-      const aTokensRemoved = oEvent.getParameter('removedTokens')[0];
-      const aSelectedTokens = this.byId('multiInputWithValueHelp').getTokens();
-      const updatedTokens = aSelectedTokens.filter((token) => token.sId !== aTokensRemoved.sId);
-      this.byId('multiInputWithValueHelp').setTokens(updatedTokens);
-    },
-  });
-});
+        if (aSelectedProducts.length === 1) {
+          MessageBox.confirm(sFormattedTextSingleConfirmation, {
+            action: [MessageBox.Action.OK],
+            onClose: (sAction) => {
+              if (sAction === MessageBox.Action.OK) {
+                oProductModel.setProperty('/Products', aProducts),
+                  oProductTable.removeSelections(true);
+              }
+            },
+          });
+        } else {
+          MessageBox.confirm(sFormattedTextMultiConfirmation, {
+            action: [MessageBox.Action.OK],
+            onClose: (sAction) => {
+              if (sAction === MessageBox.Action.OK) {
+                oProductModel.setProperty('/Products', aProducts),
+                  oProductTable.removeSelections(true);
+              }
+            },
+          });
+        }
+      },
+    });
+  },
+);
--- webapp/controller/ProductDetails.controller.js
+++ webapp/controller/ProductDetails.controller.js
@@ -1,6 +1,11 @@
 sap.ui.define(
-  ['productsreport/controller/BaseController', 'sap/ui/model/json/JSONModel'],
-  (BaseController, JSONModel) => {
+  [
+    'productsreport/controller/BaseController',
+    'sap/ui/model/json/JSONModel',
+    'sap/m/MessageBox',
+    'productsreport/utils/formatter',
+  ],
+  (BaseController, JSONModel, MessageBox, Formatter) => {
     'use strict';
 
     return BaseController.extend('productsreport.controller.ProductDetails', {
@@ -16,6 +21,9 @@ sap.ui.define(
       },
 
       onPatternMatched(oEvent) {
+        this.byId('deleteSelectedProductButton').setEnabled(true);
+        this.byId('editSelectedProductButton').setEnabled(true);
+        const oProductInfo = this.byId('productInfo');
         const sProductId = oEvent.getParameter('arguments').productId;
         const oSelectedProduct = this.getView()
           .getModel('data')
@@ -23,6 +31,204 @@ sap.ui.define(
           .find((oProduct) => oProduct.ProductId === `${sProductId}`);
         const oViewModel = this.getView().getModel('viewModel');
         oViewModel.setProperty('/selectedProduct', oSelectedProduct);
+        const oProductEdit = this.byId('productEdit').setVisible(false);
+        oProductInfo.setVisible(true);
+      },
+
+      onDeleteProductPress() {
+        const sProductDeleteConfirmationText = this.getView()
+          .getModel('i18n')
+          .getResourceBundle()
+          .getText('productDeleteConfirmation');
+
+        const aProductModel = this.getView().getModel('data').getProperty('/Products');
+        const oSelectedProductModel = this.getView()
+          .getModel('viewModel')
+          .getProperty('/selectedProduct');
+
+        const sFormattedTextSingleConfirmation = Formatter.formatProductsCount(
+          sProductDeleteConfirmationText,
+          oSelectedProductModel.ProductName,
+        );
+
+        MessageBox.confirm(sFormattedTextSingleConfirmation, {
+          action: [MessageBox.Action.OK],
+          onClose: (sAction) => {
+            if (sAction === MessageBox.Action.OK) {
+              const aFilteredProductsModel = aProductModel.filter(
+                (oProductModel) => oProductModel.ProductId !== oSelectedProductModel.ProductId,
+              );
+              this.getView().getModel('data').setProperty('/Products', aFilteredProductsModel);
+              this._navigate('ListReport');
+            }
+          },
+        });
+      },
+
+      async onEditPress() {
+        this.byId('deleteSelectedProductButton').setEnabled(false);
+        this.byId('editSelectedProductButton').setEnabled(false);
+        const oSelectedProduct = this.getView()
+          .getModel('viewModel')
+          .getProperty('/selectedProduct');
+
+        const oProductInfo = this.byId('productInfo').setVisible(false);
+        const oProductEdit = this.byId('productEdit').setVisible(true);
+
+        // if (!this.oFragmentEdit) {
+        //   this.oFragmentEdit = await this.loadFragment({
+        //     name: 'productsreport.fragment.EditProductInfo',
+        //   });
+        // }
+        // oProductInfo.addItem(this.oFragmentEdit);
+
+        const oMultiInputEdit = this.byId('multiInputWithValueHelpEdit');
+
+        oMultiInputEdit.destroyTokens();
+
+        const aSelectedProductTokens = [];
+
+        if (Array.isArray(oSelectedProduct.ProducerId)) {
+          oSelectedProduct.ProducerId.forEach((sProducerId, index) => {
+            const Token = new sap.m.Token({
+              key: sProducerId,
+              text: oSelectedProduct.ProducerName[index],
+              selected: true,
+            });
+            aSelectedProductTokens.push(Token);
+          });
+        } else {
+          const Token = new sap.m.Token({
+            key: oSelectedProduct.ProducerId,
+            text: oSelectedProduct.ProducerName,
+            selected: true,
+          });
+          aSelectedProductTokens.push(Token);
+        }
+
+        oMultiInputEdit.setTokens(aSelectedProductTokens);
+      },
+
+      async handleValueHelpEdit() {
+        const oSelectedProduct = this.getView()
+          .getModel('viewModel')
+          .getProperty('/selectedProduct');
+        const oMultiInputEdit = this.byId('multiInputWithValueHelpEdit');
+
+        if (!this.oDialogEdit) {
+          this.oDialogEdit = await this.loadFragment({
+            name: 'productsreport.fragment.ValueHelpDialog',
+          });
+          const oTable = new sap.ui.table.Table({
+            visibleRowCount: 10,
+            selectionMode: 'Multi',
+          });
+          oTable.addColumn(
+            new sap.ui.table.Column({
+              label: new sap.m.Label({ text: 'Producer Name' }),
+              template: new sap.m.Text({ text: '{data>ProducerName}' }),
+            }),
+          );
+
+          oTable.bindRows('data>/Producers');
+
+          const Tokens = oMultiInputEdit.getTokens();
+          this.oDialogEdit.setTokens(Tokens);
+          this.oDialogEdit.setTable(oTable);
+        }
+
+        this.oDialogEdit.update();
+        this.oDialogEdit.open();
+      },
+
+      onMultiInputTokenUpdate(oEvent) {
+        const oSelectedProduct = this.getView()
+          .getModel('viewModel')
+          .getProperty('/selectedProduct');
+        const oTokensRemoved = oEvent.getParameter('removedTokens')[0];
+        const aSelectedTokens = this.byId('multiInputWithValueHelpEdit').getTokens();
+        const aUpdatedTokens = aSelectedTokens.filter((token) => token.sId !== oTokensRemoved.sId);
+
+        const aProducerId = [];
+        const aProducerName = [];
+
+        if (Array.isArray(aUpdatedTokens) && aUpdatedTokens.length >= 1) {
+          aUpdatedTokens.forEach((oToken) => {
+            aProducerId.push(oToken.getKey());
+            aProducerName.push(oToken.getText());
+            oSelectedProduct.ProducerName = aProducerName;
+            oSelectedProduct.ProducerId = aProducerId;
+          });
+        } else if (aUpdatedTokens.length === 0) {
+          oSelectedProduct.ProducerName = '';
+          oSelectedProduct.ProducerId = '';
+        } else {
+          oSelectedProduct.ProducerName = aUpdatedTokens.getText();
+          oSelectedProduct.ProducerId = aUpdatedTokens.getKey();
+        }
+        this.getView().getModel('viewModel').refresh();
+        this.byId('multiInputWithValueHelpEdit').setTokens(aUpdatedTokens);
+      },
+
+      onValueHelpOkPress(oEvent) {
+        const aTokens = oEvent.getParameter('tokens');
+        const oMultiInput = this.byId('multiInputWithValueHelpEdit');
+        const oSelectedProductModel = this.getView()
+          .getModel('viewModel')
+          .getProperty('/selectedProduct');
+
+        const aTokensNormalizedText = aTokens.map((oToken) =>
+          oToken.setText(oToken.getText().slice(0, -4)),
+        );
+
+        const aProducerName = [];
+        const aProducerId = [];
+
+        aTokensNormalizedText.forEach((oToken) => {
+          aProducerName.push(oToken.getText());
+          aProducerId.push(oToken.getKey());
+        });
+
+        oSelectedProductModel.ProducerId = aProducerId;
+        oSelectedProductModel.ProducerName = aProducerName;
+
+        this.getView().getModel('viewModel').refresh();
+        oMultiInput.setTokens(aTokensNormalizedText);
+        this.oDialogEdit.setTokens(aTokensNormalizedText);
+        this.oDialogEdit.close();
+      },
+
+      onCloseValueHelp() {
+        this.oDialogEdit.close();
+      },
+
+      onAfterCloseValueHelp() {
+        this.oDialogEdit.setTokens([]);
+      },
+
+      onSelectionChange(oEvent) {
+        const aSelectedItems = oEvent.getSource().getSelectedItems();
+        const aSelectedKeys = oEvent.getSource().getSelectedKeys();
+
+        const oSelectedProduct = this.getView()
+          .getModel('viewModel')
+          .getProperty('/selectedProduct');
+
+        let aCategoryName = [];
+        const aCategoryId = [];
+
+        if (aSelectedItems.length >= 1) {
+          aSelectedItems.forEach((oSelectedItems) => {
+            aCategoryName.push(oSelectedItems.getText());
+          });
+        } else {
+          aCategoryName = '';
+        }
+
+        oSelectedProduct.Category = aCategoryName;
+        this.getView().getModel('viewModel').refresh();
+
+        const oComboBox = this.byId('comboBoxEditProducts');
       },
     });
   },
